7a9a437e4c20ed52e43394172e0db33eb4dbc060
munjalchirag7500 <munjalchirag7500@gmail.com>
2020-02-06 04:19:22 +0530

    06-02-2020(2)

--- src/app/shop/dashboard/dashboard.component.ts
+++ src/app/shop/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,54 +7,53 @@ import { FormGroup,FormBuilder,Validators} from '@angular/forms';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public show:boolean=false;
-  form:FormGroup;
-  
-  constructor(private fb:FormBuilder) { }
-  addTodo(){
-    this.show=!this.show;
+  public show: boolean = false;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+  addTodo() {
+    this.show = !this.show;
   }
   ngOnInit() {
-    this.show=false;
+    this.show = false;
     this.validate();
-    
+
 
   }
   taskDetail;
-   todo:string[]=[];
-   progress:string[]=[];
-   completed:string[]=[];
-  addtask(){
-    this.taskDetail=this.form.getRawValue();
+  todo: string[] = [];
+  progress: string[] = [];
+  completed: string[] = [];
+  addtask() {
+    this.taskDetail = this.form.getRawValue();
     console.log(this.form.getRawValue());
-    var taskData=this.form.getRawValue();
-    
-    if(taskData.category=="progress"){
-     let progressData= taskData.task;
-     this.progress.push(progressData);
-     console.log(this.progress);
-    } 
-    if(taskData.category=="todo"){
-      let todoData= taskData.task;
+    var taskData = this.form.getRawValue();
+      
+    if (taskData.category == "progress") {
+      let progressData = taskData.task;
+      this.progress.push(progressData);
+      console.log(this.progress);
+    }
+    if (taskData.category == "todo") {
+      let todoData = taskData.task;
       this.todo.push(todoData);
       console.log(this.todo);
-     }
-     if(taskData.category=="completed"){
-      let completedData= taskData.task;
+    }
+    if (taskData.category == "completed") {
+      let completedData = taskData.task;
       this.completed.push(completedData);
       console.log(this.completed);
-     }
-     
+    }
+
   }
 
-  validate()
-  {
-  	this.form=this.fb.group({
-  		'task'			:['',Validators.required],
-  		'category'   :['',Validators.required]
-  	})
+  validate() {
+    this.form = this.fb.group({
+      'task': ['', Validators.required],
+      'category': ['', Validators.required]
+    })
   }
 
-  
+
 
 }
